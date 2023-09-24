aa6d7415826597beea3baf12df25779c155be908
yuhanyang <yuhanyang@futunn.com>
2023-09-24 19:33:58 +0800

    测试consul服务发现

--- serverplugin/consul_test.go
+++ serverplugin/consul_test.go
@@ -1,15 +1,19 @@
 package serverplugin
 
 import (
+	"context"
+	"fmt"
+	metrics "github.com/rcrowley/go-metrics"
+	rpcx_consul_client "github.com/rpcxio/rpcx-consul/client"
+	"github.com/smallnest/rpcx/client"
+	"github.com/smallnest/rpcx/server"
 	"testing"
 	"time"
-
-	metrics "github.com/rcrowley/go-metrics"
-	"github.com/smallnest/rpcx/server"
 )
 
 func TestConsulRegistry(t *testing.T) {
 	s := server.NewServer()
+
 	r := NewConsulRegisterPlugin(
 		WithConsulServiceAddress("tcp@127.0.0.1:8972"),
 		WithConsulServers([]string{"127.0.0.1:8500"}),
@@ -21,17 +25,34 @@ func TestConsulRegistry(t *testing.T) {
 	if err != nil {
 		return
 	}
+	defer func() {
+		s.Close()
+		r.Stop()
+	}()
 	s.Plugins.Add(r)
 
 	s.RegisterName("Arith", new(Arith), "")
-	go s.Serve("tcp", "127.0.0.1:8972")
-	defer s.Close()
+	s.Serve("tcp", "127.0.0.1:8972")
 
-	if len(r.Services) != 1 {
-		t.Fatal("failed to register services in consul")
+	//if err := r.Stop(); err != nil {
+	//	t.Fatal(err)
+	//}
+}
+
+func TestConsulDiscovery(t *testing.T) {
+	d, _ := rpcx_consul_client.NewConsulDiscovery("/rpcx_test", "Arith", []string{"127.0.0.1:8500"}, nil)
+	xclient := client.NewXClient("Arith", client.Failtry, client.RandomSelect, d, client.DefaultOption)
+	args := Args{
+		A: 1,
+		B: 2,
 	}
-
-	if err := r.Stop(); err != nil {
-		t.Fatal(err)
+	reply := new(Reply)
+	for {
+		err := xclient.Call(context.Background(), "Mul", args, reply)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Println(reply.C)
+		time.Sleep(time.Second)
 	}
 }
